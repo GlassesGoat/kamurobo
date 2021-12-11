ff279eebe436315991f30db4cf9d2891d6407fe3
kozyonikki <45909964+GlassesGoat@users.noreply.github.com>
2021-12-12 02:48:02 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
+echo 14 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio14/direction
 while :
 do
-	echo 方向を入力してください
-	read key
-
-	if [ "$key" = "s" ]; then
-		echo まっすぐ進む
-	elif [ "$key" = "d" ]; then
-		echo　右に回る
-	elif [ "$key" = "a" ]; then
-		echo　左に回る
-	elif [ "$key" = "f" ]; then
-	       	echo　バック
-	elif [ "$key" = "" ]; then
-		echo　停止処理
-	fi
+        echo 1 > /sys/class/gpio/gpio14/value
+        sleep 1
+        echo 0 > /sys/class/gpio/gpio14/value
+        sleep 1
 done
-
