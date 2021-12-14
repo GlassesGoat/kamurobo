5f94d745feceecac936c1826f954f48e106e0f47
kozyonikki <45909964+GlassesGoat@users.noreply.github.com>
2021-12-15 00:32:30 +0900

    Update robo.sh

--- robo.sh
+++ robo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#a1=15
+#a2=14
+#b1=18
+#b2=23
+
 echo 14 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio14/direction
 echo 15 > /sys/class/gpio/export
@@ -16,38 +21,38 @@ do
 
 	if [ "$key" = "s" ]; then
 		echo まっすぐ進む
-    echo 1 > /sys/class/gpio/gpio14/value
-    echo 1 > /sys/class/gpio/gpio15/value
-    echo 0 > /sys/class/gpio/gpio18/value
-    echo 0 > /sys/class/gpio/gpio23/value
+		echo 1 > /sys/class/gpio/gpio14/value
+    		echo 0 > /sys/class/gpio/gpio15/value
+    		echo 1 > /sys/class/gpio/gpio18/value
+    		echo 0 > /sys/class/gpio/gpio23/value 
     
 	elif [ "$key" = "d" ]; then
-		echo　右に回る
-    echo 1 > /sys/class/gpio/gpio14/value
-    echo 0 > /sys/class/gpio/gpio15/value
-    echo 0 > /sys/class/gpio/gpio18/value
-    echo 0 > /sys/class/gpio/gpio23/value
+		echo　右に旋回
+    		echo 0 > /sys/class/gpio/gpio14/value
+    		echo 1 > /sys/class/gpio/gpio15/value
+    		echo 1 > /sys/class/gpio/gpio18/value
+    		echo 0 > /sys/class/gpio/gpio23/value
     
 	elif [ "$key" = "a" ]; then
-		echo　左に回る
-    echo 0 > /sys/class/gpio/gpio14/value
-    echo 1 > /sys/class/gpio/gpio15/value
-    echo 0 > /sys/class/gpio/gpio18/value
-    echo 0 > /sys/class/gpio/gpio23/value
+		echo　左に旋回
+    		echo 1 > /sys/class/gpio/gpio14/value
+    		echo 0 > /sys/class/gpio/gpio15/value
+    		echo 0 > /sys/class/gpio/gpio18/value
+    		echo 1 > /sys/class/gpio/gpio23/value
     
 	elif [ "$key" = "f" ]; then
-	  echo　バック
-    echo 0 > /sys/class/gpio/gpio14/value
-    echo 0 > /sys/class/gpio/gpio15/value
-    echo 1 > /sys/class/gpio/gpio18/value
-    echo 1 > /sys/class/gpio/gpio23/value
+		echo　バック
+	  	echo 0 > /sys/class/gpio/gpio14/value
+		echo 1 > /sys/class/gpio/gpio15/value
+	  	echo 0 > /sys/class/gpio/gpio18/value
+	  	echo 1 > /sys/class/gpio/gpio23/value
     
 	elif [ "$key" = "" ]; then
 		echo　停止処理
-    echo 0 > /sys/class/gpio/gpio14/value
-    echo 0 > /sys/class/gpio/gpio15/value
-    echo 0 > /sys/class/gpio/gpio18/value
-    echo 0 > /sys/class/gpio/gpio23/value
+		echo 0 > /sys/class/gpio/gpio14/value
+		echo 0 > /sys/class/gpio/gpio15/value
+		echo 0 > /sys/class/gpio/gpio18/value
+		echo 0 > /sys/class/gpio/gpio23/value
     
 	fi
 done
