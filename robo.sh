99eb1fdd3535c3316f4058cf214e9961521ac6b9
kozyonikki <dummy@hoge.com>
2021-12-19 00:53:19 +0900

    This is a code

--- robo.sh
+++ robo.sh
@@ -20,39 +20,39 @@ do
 	read key
 
 	if [ "$key" = "s" ]; then
-		echo まっすぐ進む
+		echo "まっすぐ進む"
 		echo 1 > /sys/class/gpio/gpio14/value
     		echo 0 > /sys/class/gpio/gpio15/value
     		echo 1 > /sys/class/gpio/gpio18/value
     		echo 0 > /sys/class/gpio/gpio23/value 
     
 	elif [ "$key" = "d" ]; then
-		echo　右に旋回
+		echo "右に旋回"
     		echo 0 > /sys/class/gpio/gpio14/value
     		echo 1 > /sys/class/gpio/gpio15/value
     		echo 1 > /sys/class/gpio/gpio18/value
     		echo 0 > /sys/class/gpio/gpio23/value
     
 	elif [ "$key" = "a" ]; then
-		echo　左に旋回
+		echo "左に旋回" 
     		echo 1 > /sys/class/gpio/gpio14/value
     		echo 0 > /sys/class/gpio/gpio15/value
     		echo 0 > /sys/class/gpio/gpio18/value
     		echo 1 > /sys/class/gpio/gpio23/value
     
 	elif [ "$key" = "f" ]; then
-		echo　バック
+		echo "バック" 
 	  	echo 0 > /sys/class/gpio/gpio14/value
 		echo 1 > /sys/class/gpio/gpio15/value
 	  	echo 0 > /sys/class/gpio/gpio18/value
 	  	echo 1 > /sys/class/gpio/gpio23/value
     
 	elif [ "$key" = "" ]; then
-		echo　停止処理
+		echo "停止処理"
 		echo 0 > /sys/class/gpio/gpio14/value
 		echo 0 > /sys/class/gpio/gpio15/value
 		echo 0 > /sys/class/gpio/gpio18/value
 		echo 0 > /sys/class/gpio/gpio23/value
-    
+
 	fi
 done
